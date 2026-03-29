034fd4dd985d7343dd187eaa43ffa97c9e6895da
Conditioned Reflex Builder <zinospetrel@sdf.org>
2026-03-29 15:33:58 +0700

    Add files via upload

---
+++ initial/0.1.1/wpdk/wpdk/wpdk-unit.php
@@ -0,0 +1,16 @@
+<?php
+
+function &wpdku_html($url) {
+  $html = file_get_contents($url);
+  $dom = new DOMDocument();
+  @$dom->loadHTML($html, LIBXML_NOWARNING | LIBXML_NOERROR);
+  return $dom;
+}
+
+function wpdku_find_by_class($dom, $classname) {
+  $xpath = new DOMXPath($dom);
+  $elements = $xpath->query("/" . "/" . "*[contains(concat(' ', normalize-space(@class), ' '), ' " . $classname . " ')]");
+  return $elements;
+}
+
+?>
