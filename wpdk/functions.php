bdf46d29fb279aef6476abe8ebb141dea8da6cb9
Conditioned Reflex Builder <zinospetrel@sdf.org>
2026-04-06 20:12:14 +0700

    Add files via upload

---
+++ wpdk/functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright (c) 2026 by Dinh Thoai Tran <zinospetrel@sdf.org>.
+ * All rights reserved.
+ *
+ * Source code of 'Wordpress On Disk' (wpdk) theme is licensed under the GPLv2.
+ *
+ * Viewing copyright information at https://github.com/condlex/wpdk/LICENSE .
+ */
+
+wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all' );
