0f34444851bf6e85feef9ecba440af27851450c2
Conditioned Reflex Builder <zinospetrel@sdf.org>
2026-04-09 02:51:42 +0700

    Add files via upload

--- wpdk/patterns/wpdk-admin-search-template.php
+++ wpdk/patterns/wpdk-admin-search-template.php
@@ -23,14 +23,74 @@ function get_param( $key ) {
   return '';
 }
 
-function summarize( $content, $summary_size ) {
-  $summary = $content;
-  if ( strlen( $summary ) < $summary_size ) {
-    return $summary;
+function marking_title( $title, $word_list ) {
+  foreach ( $word_list as $word ) {
+    $word_size = strlen( $word );
+    $n_title = '';
+    $start = 0;
+    $position = stripos( $title, $word, $start );
+    while ( $position !== false ) {
+      $n_title .= substr( $title, $start, $position - $start ) . '<span class="wpdk-mark" style="color: red">' . substr( $title, $position, $word_size ) . '</span>';
+      $start = $position + $word_size;
+      $position = stripos( $title, $word, $start );
+    }
+    $n_title .= substr( $title, $start );
+    $title = $n_title;
   }
-  $position = strpos( $summary, ' ', $summary_size );
-  $summary = substr( $summary, 0, $position );
-  return $summary;
+  return $title;
+}
+
+function marking_content( $content, $word_list, $context_size, $max_context_count = 3 ) {
+  $start = 0;
+  $context_count = 0;
+  $new_content = '';
+  $content_size = strlen( $content );
+  $position = strlen( $content );
+  $position_word = '';
+  foreach ( $word_list as $word ) {
+    $new_position = stripos( $content, $word, $start );
+    if ( $new_position !== false ) {
+      if ( $new_position < $position ) {
+        $position = $new_position;
+        $position_word = $word;
+      }
+    }
+  }
+  while ( $position_word !== '' && $context_count < $max_context_count ) {
+    $context_count++;
+    $begin = $position - $context_size;
+    if ( $begin <= $start ) {
+      $begin = $start;
+    }
+    $new_position = strpos( $content, ' ', $begin );
+    if ( $new_position !== false ) {
+      $begin = $new_position;
+    }
+
+    $end = $position + $context_size;
+    if ( $end >= $content_size - 1 ) {
+      $end = $content_size - 1;
+    }
+    $new_position = strpos( $content, ' ', $end - 10 );
+    if ( $new_position !== false ) {
+      $end = $new_position;
+    }
+    $new_content .= '<div class="wpdk-cutting" style="padding-top: 5px; padding-bottom: 5px; border-bottom: dotted 1px gainsboro;">' . marking_title( substr( $content, $begin, $end - $begin + 1 ), $word_list ) . '</div>';
+    $start = $end;
+
+    $position = strlen( $content );
+    $position_word = '';
+    foreach ( $word_list as $word ) {
+      $new_position = stripos( $content, $word, $start );
+      if ( $new_position !== false ) {
+        if ( $new_position < $position ) {
+          $position = $new_position;
+          $position_word = $word;
+        }
+      }
+    }
+  }
+  return $new_content;
 }
 
 $available = true;
@@ -43,7 +103,7 @@ if ( $available ) {
 }
 
 if ( $available ) {
-  $summary_size = 500;
+  $context_size = 100;
   $current_uri = $_SERVER['REQUEST_URI'];
   $position = strrpos( $current_uri, '?' );
   if ( $position !== false ) {
@@ -66,15 +126,63 @@ if ( $available ) {
   $post_list = [];
   $searching = false;
   $page_list = [ 1 ];
-  if ( strlen( $query ) > 0 ) {
+  if ( strlen( $query ) === 0 ) {
+    $searching = true;
+    $content_size = 300;
+    $id_list = wpdk_search_list_all();
+    for ( $index = $from_count; $index < $to_count; $index++ ) {
+      $id = $id_list[ $index ];
+      $post = wpdk_load_post( $id );
+      if ( $post === false ) continue;
+      $content = $post['content'];
+      if ( strlen( $content ) > $content_size ) {
+        $position = strpos( $content, ' ', $content_size - 10 );
+        if ( $position === false ) {
+          $content = substr( $content, 0, $content_size );
+        } else {
+          $content = substr( $content, 0, $position );
+        }
+      }
+      $post['content'] = $content;
+      $post_list[] = $post;
+    }
+    $page_list = [];
+    $post_count = count( $id_list );
+    $post_count_less = $post_count - 1;
+    $page_count = ( ( $post_count_less - ( $post_count_less % $page_size ) ) / $page_size ) + 1;
+    for ( $no = 1; $no <= 5; $no++ ) {
+      if ( $no < 1 || $no > $page_count ) continue;
+      if ( in_array( $no, $page_list ) ) continue;
+      $page_list[] = $no;
+    }
+    for ( $no = $page_no - 5; $no <= $page_no + 5; $no++ ) {
+      if ( $no < 1 || $no > $page_count ) continue;
+      if ( in_array( $no, $page_list ) ) continue;
+      $page_list[] = $no;
+    }
+    for ( $no = $page_count - 5; $no <= $page_count; $no++ ) {
+      if ( $no < 1 || $no > $page_count ) continue;
+      if ( in_array( $no, $page_list ) ) continue;
+      $page_list[] = $no;
+    }
+  } else {
+    $word_list_r = explode( ' ', $query );
+    $word_list = [];
+    foreach ( $word_list_r as $word ) {
+      $word = trim( $word );
+      if ( $word === '' ) continue;
+      $word_list[] = strtolower( $word );
+    }
     $searching = true;
     $id_list = wpdk_search_simple_query( $query );
     for ( $index = $from_count; $index < $to_count; $index++ ) {
       $id = $id_list[ $index ];
       $post = wpdk_load_post( $id );
       if ( $post === false ) continue;
-      $summary = summarize( $post['content'], $summary_size );
-      $post['summary'] = $summary;
+      $content = marking_content( esc_html( $post['content'] ), $word_list, $context_size );
+      $post['content'] = $content;
+      $title = marking_title( esc_html( $post['title'] ), $word_list );
+      $post['title'] = $title;
       $post_list[] = $post;
     }
     $page_list = [];
@@ -127,9 +235,9 @@ if ( $available ) {
         <div class="wpdk-search-item">
           <div class="wpdk-search-title">
             <?php echo ( $from_count + $index ); ?>.
-            <a style="color: black" target="_blank" href="<?php echo $current_uri_normal . '?wpdk=y&p=' . $post['id']; ?>"><?php echo esc_html( $post['title'] ); ?></a>
+            <a style="color: black;" target="_blank" href="<?php echo $current_uri . '?wpdk=y&p=' . $post['id']; ?>"><?php echo $post['title']; ?></a>
           </div>
-          <div class="wpdk-search-summary"><?php echo esc_html( $post['summary'] ); ?></div>
+          <div class="wpdk-search-summary"><?php echo $post['content']; ?></div>
           <div class="wpdk-search-widget" style="margin-top: 5px">
             <a style="text-decoration: none; color: black;" class="wpdk-button" href="<?php echo $current_uri . '?post_type=postisk&edit=y&id=' . $post['id']; ?>">Edit</a>
           </div>
